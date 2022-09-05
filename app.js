80e7436a50d57b3d71e4842c21a9880ba4258bf1
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2022-09-04 22:47:11 -0300

    Utilizando apenas um modal alert

--- app.js
+++ app.js
@@ -63,12 +63,30 @@ function cadastrarDespesa() {
 
   if (despesa.validarDados()) {
     bd.gravar(despesa);
+    document.getElementById("modal_titulo").innerHTML =
+      "Registro inserido com sucesso";
+    document.getElementById("modal_titulo_div").className =
+      "modal-header text-success";
+    document.getElementById("modal_conteudo").innerHTML =
+      "Despesa foi cadastrada com sucesso!";
+    document.getElementById("modal_btn").innerHTML = "Voltar";
+    document.getElementById("modal_btn").className = "btn btn-success";
+
     //dialog de sucesso
-    console.log("Dados válidos")
-    $("#sucessoGravacao").modal("show")
+    console.log("Dados válidos");
+    $("#modalRegistraDespesa").modal("show");
   } else {
+    document.getElementById("modal_titulo").innerHTML =
+      "Erro na inclusão do registro";
+    document.getElementById("modal_titulo_div").className =
+      "modal-header text-danger";
+    document.getElementById("modal_conteudo").innerHTML =
+      "Erro na gravação, verifique se todos os campos foram preenchidos corretamente.";
+    document.getElementById("modal_btn").innerHTML = "Voltar e corrigir";
+    document.getElementById("modal_btn").className = "btn btn-danger";
+
     //dialog de erro
-    console.log("Dados Inválidos")
-    $("#erroGravacao").modal("show")
+    console.log("Dados Inválidos");
+    $("#modalRegistraDespesa").modal("show");
   }
 }
